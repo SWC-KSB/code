e2bf8053170b3c17256792139b4c3ebe7da0fc94
SHI0284 <gpdls0411@naver.com>
2024-09-26 01:19:08 +0900

    너무너무 힘들어요

--- Assets/Scripts/UI/Skill_Key.cs
+++ Assets/Scripts/UI/Skill_Key.cs
@@ -3,39 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Skill_Key : MonoBehaviour
 {
     public Image img_Skill_1;
+    private bool isCoolingDown = false; // 쿨타임 상태를 관리할 변수
 
     // Use this for initialization
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Q))
+        // 쿨타임이 돌아가는 중이 아니라면 키 입력을 받음
+        if (!isCoolingDown && Input.GetKeyUp(KeyCode.Q))
         {
             StartCoroutine(CoolTime(3f));
         }
     }
 
-    IEnumerator CoolTime (float cool)
+    IEnumerator CoolTime(float cool)
     {
+        isCoolingDown = true; // 쿨타임 시작
         print("쿨타임 코루틴 실행");
 
-        while (cool > 1.0f)
+        while (cool > 0.0f)
         {
             cool -= Time.deltaTime;
-            img_Skill_1.fillAmount = (1.0f / cool);
+            img_Skill_1.fillAmount = cool / 3.0f; // 3초 기준으로 계산
             yield return new WaitForFixedUpdate();
         }
 
+        isCoolingDown = false; // 쿨타임 종료
         print("쿨타임 코루틴 완료");
     }
-
-
 }
